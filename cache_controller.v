2caaabaa18f530c59c1d489b3be3d893a06fc09a
Ethan Yun Ku <eyunku@gmail.com>
2023-11-19 20:06:24 -0600

    add specifications

--- cache_controller.v
+++ cache_controller.v
@@ -1,4 +1,8 @@
 module cache_fill_FSM(clk, rst_n, miss_detected, miss_address, fsm_busy, write_data_array, write_tag_array, memory_address, memory_data, memory_data_valid);
+    // cache capacity: 2KB, memory: 64KB, block size: 16 bytes
+    // data transfer granularity: 2-byte words, memory access latency: 4 cycles
+    // write-through policy, stall on miss until whole cache block is brought into cache
+    
     input clk, rst_n;
     input miss_detected; // active high when tag match logic detects a miss
     input [15:0] miss_address; // address that missed the cache
